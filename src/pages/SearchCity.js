906ed00f1506ba37dd17600a52cb9f371c31d2af
Remzi Güneş <remzi.gunes1283@gmail.com>
2020-09-04 17:08:42 +0300

    change color

--- src/pages/SearchCity.js
+++ src/pages/SearchCity.js
@@ -57,7 +57,7 @@ const Button = styled.button`
   padding: 0.5rem 0;
   margin: 0.5rem 1rem;
   width: 50%;
-  background: aqua;
+  background: pink;
   color: white;
   border: 2px solid white;
 `;
